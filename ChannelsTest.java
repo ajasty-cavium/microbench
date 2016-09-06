a8bb5d3a8a6701bea99e497c79f08700c75fc7a2
Shay Gal-On <shaykul@gmail.com>
2016-09-06 11:25:12 -0700

    Fix test, buffer needs to be reset after reading from it.

--- ChannelsTest.java
+++ ChannelsTest.java
@@ -18,20 +18,31 @@ public class ChannelsTest {
         
         //allocate byte buffer size
         ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
+		long buffers=0,bytes=0;
+		Report.start();
+	int nread,nfree;
         
-        while(channel.read(byteBuffer) > 0){
+        while((nread=channel.read(byteBuffer)) > 0){
             
             //limit is set to current position and position is set to zero
             byteBuffer.flip();
+			buffers++;
             
             while(byteBuffer.hasRemaining()){
                 char ch = (char) byteBuffer.get();
+		bytes++;
               //  System.out.print(ch);
             }
+	    byteBuffer.clear();
         }
+		Report.end();
+		Report.report("ChannelTest",buffers);
       }catch(IOException e){
           e.printStackTrace();
+      }catch(Exception e){
+          e.printStackTrace();
       }finally{
+	System.out.println("Finally");
           in.close();
           channel.close();
       }
