51a5599883339c5b19bd84695b8412288841dd94
Shay Gal-On <shaykul@gmail.com>
2016-09-06 20:04:45 -0700

    More generalized reporting, added list sort

--- LinkedListExample.java
+++ LinkedListExample.java
@@ -1,45 +1,54 @@
 import java.util.*;
 public class LinkedListExample {
-     public static void main(String args[]) {
 
-         /* Linked List Declaration */
-         LinkedList<Integer> linkedlist = new LinkedList<Integer>();
+    public static void main(String args[]) {
+	Comparator<Integer> comparator = new Comparator<Integer>() {
+		public int compare(Integer c1, Integer c2) {
+			return c2 - c1; 
+		}
+	};
 
-         /*add(String Element) is used for adding 
-          * the elements to the linked list*/
+        /* Linked List Declaration */
+        LinkedList<Integer> linkedlist = new LinkedList<Integer>();
 
-         /*Display Linked List Content*/
-         System.out.println("Linked List Content: " +linkedlist);
+        /*add(String Element) is used for adding
+         * the elements to the linked list*/
 
-         /*Add First and Last Element*/
+        /*Display Linked List Content*/
+        System.out.println("Linked List Content: " +linkedlist);
 
-         /*This is how to get and set Values*/
-         System.out.println("LinkedList after deletion of first and last element: " +linkedlist);
-long start_time = System.nanoTime();
+        /*Add First and Last Element*/
 
-         /* Add to a Position and remove from a position*/
-	 Random rnd = new Random();
+        /*This is how to get and set Values*/
+        System.out.println("LinkedList after deletion of first and last element: " +linkedlist);
+		Report.start();
 
- 	for(int i=0;i<10000000;++i)
-	{
-		linkedlist.add(rnd.nextInt());
-	}
-long end_time = System.nanoTime();
-double difference = (end_time - start_time)/1e6;
+        /* Add to a Position and remove from a position*/
+        Random rnd = new Random();
 
-               System.out.println("time was " +difference);
-
-  int el=0;
-  int sum=0;
-  for(int i=0;i<10000000;++i)
+        for (int i=0;i<10000000;++i)
         {
-              el= linkedlist.pop();	
-              sum =(sum+el)%10;
+            linkedlist.add(rnd.nextInt());
         }
+		Report.end();
+		Report.report("Linked List<I>-add");
 
- start_time = System.nanoTime();
- difference = (end_time - start_time)/1e6;
- System.out.println("time was " +difference);
+		Report.start();
+		linkedlist.sort((left,right) -> Integer.compare(right,left));
+		//Collections.sort(linkedlist,comparator);
+		Report.end();
+		Report.report("Linked List<I>-sort","s");
+		
+        int el=0;
+        int sum=0;
+		Report.start();
+        for (int i=0;i<10000000;++i)
+        {
+            el= linkedlist.pop();
+            sum =(sum+el)%10;
+        }
+		Report.end();
+		Report.report("Linked List<I>-pop");
 
-     }
+    }
 }
